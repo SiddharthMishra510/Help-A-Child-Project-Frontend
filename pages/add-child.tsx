78922ee925b2bfbd628bc3c74435519b29a0e7a7
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-20 03:13:59 +0530

    feat: add a 'back to home' button to the add-child page

--- pages/add-child.tsx
+++ pages/add-child.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import AddChild from "../components/AddChild";
+import Link from "next/link";
+import { Button, Box } from "@mui/material";
 
 const AddChildPage = () => {
-    return <AddChild />;
+    return (
+        <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link href="/">
+                <Button variant="outlined" color="secondary">
+                    Back to Home
+                </Button>
+            </Link>
+            <AddChild />
+        </Box>
+    );
 };
 
 export default AddChildPage;
