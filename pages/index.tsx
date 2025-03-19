17c0a50a69c1fabb3335ccae741cae721a772365
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-03-20 02:31:22 +0530

    chore: move index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Box, Typography, Card, CardContent } from "@mui/material";
-import Carousel from "./components/Carousel";
-import {Child} from "./types/child";
+import Carousel from "../components/Carousel";
+import {Child} from "../types/child";
 
 export default function App() {
     const [children, setChildren] = useState<Child[]>([]);
