734b3748c7c592adb1eb815e6d42356c89d02cb8
SiddharthMishra510 <siddharthmishra5105@gmail.com>
2025-04-04 02:02:45 +0530

    feat: make donation page app router compatible

--- app/donate.tsx
+++ app/donate.tsx
@@ -1,14 +1,16 @@
-import { useRouter } from "next/navigation";
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import React from "react";
 import DonateForm from "../components/DonateForm";
 import Link from "next/link";
 import { Button, Box, Typography } from "@mui/material";
 
 const DonationPage = () => {
-    const router = useRouter();
-    const { childId } = router.query;
+    const searchParams = useSearchParams();
+    const childId = searchParams.get("childId");
 
-    if (!childId || typeof childId !== "string") {
+    if (!childId) {
         return <Typography color="error">Invalid child ID</Typography>;
     }
 
